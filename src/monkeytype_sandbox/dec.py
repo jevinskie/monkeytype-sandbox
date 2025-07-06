a0ae09010c1bf059c327cc51dafda532afee92e7
Jevin Sweval <jevinsweval@gmail.com>
2025-07-06 16:27:55 -0400

    wip

--- src/monkeytype_sandbox/dec.py
+++ src/monkeytype_sandbox/dec.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import typing
-from typing import Any
-from typing import Any as TEAny
+from typing import TYPE_CHECKING, Callable, cast
 
 import typing_extensions
+from typing_extensions import Any, ParamSpec, Protocol, TypeVar, reveal_type
+from typing_extensions import Any as TEAny
 
 if typing_extensions.Any is not typing.Any:
     raise TypeError
@@ -12,29 +13,26 @@ if TEAny is not Any:
     raise TypeError
 
 
-class MQN(typing_extensions.Protocol):
+_P = ParamSpec("_P")
+_R = TypeVar("_R", covariant=True)  # covariant so it can be used in return position
+
+
+class _CallableWithMetadata(Protocol[_P, _R]):
     m: str
     qn: str
 
-
-class MQNR:
-    m: str
-    qn: str
+    def __call__(self, *args: _P.args, **kwargs: _P.kwargs) -> _R: ...
 
 
-if typing_extensions.TYPE_CHECKING:
-    typing_extensions.reveal_type(MQN)
-    typing_extensions.reveal_type(MQNR)
-    # typing_extensions.reveal_type(MQN())
-    typing_extensions.reveal_type(MQNR())
-    if hasattr(MQN, "m"):
-        typing_extensions.reveal_type(MQN.m)
-    if hasattr(MQNR, "m"):
-        typing_extensions.reveal_type(MQNR.m)
-    if hasattr(MQNR(), "m"):
-        typing_extensions.reveal_type(MQNR().m)
+# def register(
+#     mod: str, qualname: str
+# ) -> Callable[[Callable[_P, _R]], _CallableWithMetadata[_P, _R]]:
+#     def decorator(func: Callable[_P, _R]) -> _CallableWithMetadata[_P, _R]:
+#         setattr(func, "m", mod)
+#         setattr(func, "qn", qualname)
+#         return cast(_CallableWithMetadata[_P, _R], func)
 
-_F = typing_extensions.TypeVar("_F", bound=typing_extensions.Callable[..., typing_extensions.Any])
+#     return decorator
 
 
 class register:
@@ -43,8 +41,57 @@ class register:
         self.m = mod
         self.qn = qualname
 
-    def __call__(self, func: _F) -> _F:
+    def __call__(self, func: Callable[_P, _R]) -> _CallableWithMetadata[_P, _R]:
         print(f"register __call__ self: {self} func: {func}")
+        setattr(func, "m", self.m)
+        setattr(func, "qn", self.qn)
+        return cast(_CallableWithMetadata[_P, _R], func)
+
+
+@register("foo", "bar")
+def cool(a: int, b: int) -> int:
+    r = a + b
+    print(f"a: {a} b: {b} a + b: {r} m: {cool.m} qn: {cool.qn}")
+    return r
+
+
+print(f"cool.m: {cool.m} cool.qn: {cool.qn}")
+print(f"cool(3, 5): {cool(3, 5)}")
+
+if TYPE_CHECKING:
+    reveal_type(cool)
+    reveal_type(cool.m)
+    reveal_type(cool.qn)
+
+
+class CoolClass:
+    @register("foo", "bar")
+    def cool_meth(a: int, b: int) -> int:
+        r = a + b
+        print(f"a: {a} b: {b} a + b: {r} m: {cool.m} qn: {cool.qn}")
+        return r
+
+
+cc = CoolClass()
+
+if TYPE_CHECKING:
+    reveal_type(CoolClass.cool_meth)
+    reveal_type(CoolClass.cool_meth.m)
+    reveal_type(CoolClass.cool_meth.qn)
+    reveal_type(cc.cool_meth)
+    reveal_type(cc.cool_meth.m)
+
+_F = TypeVar("_F", bound=Callable[..., Any])
+
+
+class register2:
+    def __init__(self, mod: str, qualname: str):
+        print(f"register2 __init__ self: {self} mod: {mod} qualname: {qualname}")
+        self.m = mod
+        self.qn = qualname
+
+    def __call__(self, func: _F) -> _F:
+        print(f"register2 __call__ self: {self} func: {func}")
         setattr(func, "m", self.m)
         setattr(func, "qn", self.qn)
         return func
@@ -56,11 +103,6 @@ class TypeRewriter:
 
 
 class BasicTypeRewriter(TypeRewriter):
-    def rewrite(self) -> None:
-        print(f"BasicTypeRewriter rewrite self: {self}")
-        self.rewrite_typing_extensions_union()
-        self.rewrite_pycp_union()
-
     @register("typing_extensions", "Union")
     def rewrite_typing_extensions_union(self) -> None:
         print(f"rewrite_typing_extensions_union self: {self}")
@@ -74,6 +116,14 @@ class BasicTypeRewriter(TypeRewriter):
         print(f"rewrite_typing_extensions_union.qn: {self.rewrite_pycp_union.qn}")
         if typing_extensions.TYPE_CHECKING:
             typing_extensions.reveal_type(self.rewrite_pycp_union.qn)
+
+    def rewrite(self) -> None:
+        print(f"BasicTypeRewriter rewrite self: {self}")
+        if TYPE_CHECKING:
+            reveal_type(self.rewrite_typing_extensions_union)
+            reveal_type(self.rewrite_pycp_union)
+        self.rewrite_typing_extensions_union()
+        self.rewrite_pycp_union()
 
 
 tr = TypeRewriter()
