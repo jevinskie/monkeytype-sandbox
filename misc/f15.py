4e0aef77259e65e4ccf0874f5658a9941027b697
Jevin Sweval <jevinsweval@gmail.com>
2025-07-11 14:09:38 -0400

    add meta param

--- misc/f15.py
+++ misc/f15.py
@@ -11,11 +11,9 @@ from typing import (
     NamedTuple,
     ParamSpec,
     Protocol,
-    Self,
     TypeVar,
     cast,
     overload,
-    reveal_type,
 )
 
 if not TYPE_CHECKING:
@@ -54,56 +52,11 @@ class AnnotatedMethodMethodCall(Protocol, Generic[_P, _R_co]):
     def __call__(self, meta: AnnotatedMethodInfo, *args: _P.args, **kwargs: _P.kwargs) -> _R_co: ...
 
 
-if TYPE_CHECKING:
-    reveal_type(AnnotatedMethodFunctionCall)
-    reveal_type(AnnotatedMethodFunctionCall.__call__)
-    reveal_type(AnnotatedMethodMethodCall)
-    reveal_type(AnnotatedMethodMethodCall.__call__)
-
-
-class A:
-    def a(self) -> int: ...
-    def b(self, x): ...
-    def c(self: Self, x: int) -> int: ...
-    def d(self, x: int, *args: Any, i: int | None = None, **kwargs: Any) -> int: ...
-    def e(self, x: int, *, i: int | None = None, **kwargs) -> int: ...
-    def f(self, x: int, /, *, i: int | None = None, **kwargs) -> int: ...
-    def f2(self, x: int, /, i: int | None = None, **kwargs) -> int: ...
-    def g(self, x: int, /, *args: Any, i: int | None = None, **kwargs) -> int: ...
-    def h(self, x: int, /, *args: Any, i: int | None = None) -> int: ...
-    def i(self, x: int, /, *args: Any) -> int: ...
-
-
-if TYPE_CHECKING:
-    a = A()
-    # reveal_type(A.a)
-    # reveal_type(a.a)
-    # reveal_type(A.b)
-    # reveal_type(a.b)
-    # reveal_type(A.c)
-    # reveal_type(a.c)
-    # reveal_type(A.d)
-    # reveal_type(a.d)
-    reveal_type(A.e)
-    reveal_type(a.e)
-    reveal_type(A.f)
-    reveal_type(a.f)
-    reveal_type(A.f2)
-    reveal_type(a.f2)
-    # reveal_type(A.g)
-    # reveal_type(a.g)
-    # reveal_type(A.h)
-    # reveal_type(a.h)
-    # reveal_type(A.i)
-    # reveal_type(a.i)
-
-
 class AnnotatedMethod(Generic[_T, _P, _R_co]):
     _np: NamePath
     _n: str
     _f: Callable[Concatenate[_T, _P], _R_co]
     _fmeta: Callable[Concatenate[_T, _P], _R_co]
-    # _mmeta:
 
     # FIXME: Need weakref?
 
@@ -118,10 +71,10 @@ class AnnotatedMethod(Generic[_T, _P, _R_co]):
     def __get__(
         self, obj: _T | None, cls: type[_T] | None = None, /
     ) -> Callable[Concatenate[_T, _P], _R_co] | Callable[_P, _R_co]:
-        meta = self.as_ntuple()
         if obj is None:
-            return partial(self._f, meta=meta)
-        return cast(Callable[_P, _R_co], self._f.__get__(obj, cls))
+            return self._fmeta
+        p = partial(self._f.__get__(obj, cls), meta=self.as_ntuple())
+        return cast(Callable[_P, _R_co], p)
 
     def __set_name__(self, obj: Any, name: str) -> None:
         self._n = name
@@ -129,7 +82,11 @@ class AnnotatedMethod(Generic[_T, _P, _R_co]):
             raise ValueError(f"None obj? {obj}")
         if not hasattr(obj, "_infos"):
             setattr(obj, "_infos", {})
-        obj._infos[self._np] = self.as_ntuple()
+        nt = self.as_ntuple()
+        obj._infos[self._np] = nt
+        # Argument "meta" has incompatible type "AnnotatedMethodInfo"; expected "_P.kwargs"
+        p = partial(self._f, meta=nt)  # type: ignore
+        self._fmeta = cast(Callable[Concatenate[_T, _P], _R_co], p)
 
     def as_ntuple(self) -> AnnotatedMethodInfo:
         return AnnotatedMethodInfo(self._np, self._n, cast(MethodType, self))
