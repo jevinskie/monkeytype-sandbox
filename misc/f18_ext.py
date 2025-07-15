beff5e1d393d9a614451b50515b6fc49ef156341
Jevin Sweval <jevinsweval@gmail.com>
2025-07-15 10:05:26 -0400

    wip

--- misc/f18_ext.py
+++ misc/f18_ext.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 import sys as sys
 import traceback
 import types
+from typing import cast
 
 print("f18_ext mod top level")
 
@@ -50,3 +53,31 @@ stuff_glob[-1] = stuff_glob[0]()
 print("f18_mod_top_frame:")
 print(f18_ext_mod_top_frame)
 f18_ext_frame_thingy(f18_ext_mod_top_frame)
+
+
+def walk_frames(frame: types.FrameType | None) -> None:
+    depth = 0
+
+    class F:
+        f_back = frame
+
+    frame = cast(types.FrameType, F())
+    depth = 0
+    while frame := frame.f_back:
+        print(f"[{depth}] name: {frame.f_code.co_name}")
+        depth += 1
+
+
+walk_frames(FE.stuff_clsvar[1][1])
+# Current:
+# [0] name: bar
+# [1] name: bar_wrapper
+# [2] name: foo
+# [3] name: FE
+# [4] name: <module>
+# Desired:
+# [0] name: bar
+# [1] name: bar_wrapper
+# [2] name: foo
+# [3] name: FE
+# [4] name: misc.f18_ext
